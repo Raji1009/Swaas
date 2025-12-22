827c667711f238e547452ef466d4f613e2c5cde9
Raji1009 <lavanis7u@gmail.com>
2025-12-22 20:44:06 +0530

    Fix Vercel SPA routing for CRA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import PatientProfile from './pages/PatientProfile';
 import Auth from './pages/Auth';
